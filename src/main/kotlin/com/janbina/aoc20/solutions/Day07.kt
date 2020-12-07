4b132c47f0ab0168ee2631ae225b1bc8c6ad573e
Jan Bína <binajohny@gmail.com>
2020-12-08 00:11:51 +0100

    Add easier but slover solution for day 7

--- src/main/kotlin/com/janbina/aoc20/solutions/Day07.kt
+++ src/main/kotlin/com/janbina/aoc20/solutions/Day07.kt
@@ -2,13 +2,26 @@ package com.janbina.aoc20.solutions
 
 import com.janbina.aoc20.utils.Input
 import com.janbina.aoc20.utils.printAndTest
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
+@ExperimentalTime
 fun main() {
     val input = Input.getDayInputLines(7)
-    Day07(input).also {
-        printAndTest(it.part1(), 112)
-        printAndTest(it.part2(), 6260)
-    }
+
+    measureTime {
+        Day07(input).also {
+            printAndTest(it.part1(), 112)
+            printAndTest(it.part2(), 6260)
+        }
+    }.also { println("Time 1 = $it") }
+
+    measureTime {
+        Day07x(input).also {
+            printAndTest(it.part1(), 112)
+            printAndTest(it.part2(), 6260)
+        }
+    }.also { println("Time 2 = $it") }
 }
 
 class Day07(input: List<String>) {
@@ -20,7 +33,7 @@ class Day07(input: List<String>) {
         val mutTrees = mutableMapOf<String, Bag>()
 
         val bags = input.map {
-            createBag(it)
+            Bag.fromString(it)
         }.associate { it.name to it.contains }
 
         fun buildTree(name: String): Bag {
@@ -68,25 +81,68 @@ class Day07(input: List<String>) {
         return trees[myBagName]?.numContains() ?: error("There's no bag named \"$myBagName\"")
     }
 
-    private fun createBag(line: String): Bag {
-        val name = line.substringBefore(" bag")
-        val rest = line.substringAfter("contain ")
-        val contains = if (rest.contains("no other")) {
-            emptyList()
-        } else {
-            rest.split(",")
-                .map {
-                    val numName = it.substringBefore(" bag").trim()
-                    val num = numName.takeWhile { it.isDigit() }.toInt()
-                    val nm = numName.substringAfter(" ")
-                    num to Bag(nm, emptyList())
-                }
-        }
-        return Bag(name, contains)
-    }
-
-    private data class Bag(
+    data class Bag(
         val name: String,
         val contains: List<Pair<Int, Bag>>
-    )
+    ) {
+        companion object {
+            fun fromString(line: String): Bag {
+                val name = line.substringBefore(" bag")
+                val rest = line.substringAfter("contain ")
+                val contains = if (rest.contains("no other")) {
+                    emptyList()
+                } else {
+                    rest.split(",")
+                        .map {
+                            val numName = it.substringBefore(" bag").trim()
+                            val num = numName.takeWhile { it.isDigit() }.toInt()
+                            val nm = numName.substringAfter(" ")
+                            num to Bag(nm, emptyList())
+                        }
+                }
+                return Bag(name, contains)
+            }
+        }
+    }
+}
+
+// easier and more concise solution, without all the caching that should take more time for larger input
+// for provided input, the difference is like 55ms to 550ms
+class Day07x(input: List<String>) {
+
+    private val myBagName = "shiny gold"
+    private val trees: List<Day07.Bag>
+
+    init {
+        val bags = input.map { Day07.Bag.fromString(it) }
+
+        fun buildTree(name: String): Day07.Bag {
+            val subtrees = bags.first { it.name == name }.contains.map {
+                it.first to buildTree(it.second.name)
+            }
+            return Day07.Bag(name, subtrees)
+        }
+
+        trees = bags.map { buildTree(it.name) }
+    }
+
+    fun part1(): Int {
+        return trees.count {
+            it.canHold(myBagName)
+        }
+    }
+
+    fun part2(): Int {
+        return trees.first { it.name == myBagName }.numContains()
+    }
+
+    private fun Day07.Bag.canHold(what: String): Boolean {
+        return contains.any { it.second.name == what || it.second.canHold(what) }
+    }
+
+    private fun Day07.Bag.numContains(): Int {
+        return contains.sumBy {
+            it.first * (it.second.numContains() + 1)
+        }
+    }
 }
